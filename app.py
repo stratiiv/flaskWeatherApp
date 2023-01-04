31c3185bd610b3b2764eb04e896e71e97480538c
Valentyn Stratii <stratiyvalter@gmail.com>
2023-01-04 18:41:44 +0200

    add main functionality

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask,render_template,request,redirect,url_for,flash
+from flask_sqlalchemy import SQLAlchemy
+import requests
+import pprint
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config['SECRET_KEY'] = 'secretkey'
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+def get_weather_data(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=7598a17dae98564b0adfa271f40a66e7'
+    r = requests.get(url).json()
+    return r
+
+@app.route('/')
+def index_get():
+    cities = City.query.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = get_weather_data(city.name)
+
+        weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(weather)
+
+
+    return render_template('index.html', weather_data=weather_data)
+
+@app.route('/', methods=['POST'])
+def index_post():
+    err_msg = ''
+    new_city = request.form.get('city')
+        
+    if new_city:
+        existing_city = City.query.filter_by(name=new_city).first()
+
+        if not existing_city:
+            new_city_data = get_weather_data(new_city)
+
+            if new_city_data['cod'] == 200:
+                new_city_obj = City(name=new_city)
+
+                db.session.add(new_city_obj)
+                db.session.commit()
+            elif new_city_data['cod']=='404':
+                err_msg = 'City does not exist in the world!'
+        else:
+            err_msg = 'City already exists in the database!'
+
+    if err_msg:
+        flash(err_msg, 'error')
+    else:
+        flash('City added succesfully!')
+
+    return redirect(url_for('index_get'))
+
+@app.route('/delete/<name>')
+def delete_city(name):
+    city = City.query.filter_by(name=name).first()
+    db.session.delete(city)
+    db.session.commit()
+
+    flash(f'Successfully deleted { city.name }', 'success')
+    return redirect(url_for('index_get'))
+
+if __name__=='__main__':
+    app.run(host='127.0.0.1',port=5000,debug=True)
